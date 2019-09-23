2a57fcd5968a069fca4536757bd2c58e2e7b6e0f
Pitrified <nobilipietro@gmail.com>
2019-09-23 17:57:41 +0200

    Add script to setup xfce4 in WSL

---
+++ install_wsl.sh
@@ -0,0 +1,16 @@
+# setup desktop environment for WSL
+# https://github.com/QMonkey/wsl-tutorial
+
+echo ¨¨ >> ~/.bash_aliases.local
+echo ¨# WSL¨ >> ~/.bash_aliases.local
+echo ¨export DISPLAY=:0.0¨ >> ~/.bash_aliases.local
+echo ¨export LIBGL_ALWAYS_INDIRECT=1¨ >> ~/.bash_aliases.local
+
+sudo apt install xfce4-terminal
+sudo apt install xfce4
+
+# fix missing themes
+# https://elementaryos.stackexchange.com/a/3208
+sudo apt install gtk2-engines-pixbuf
+
+echo ¨Use startxfce4 to launch¨
