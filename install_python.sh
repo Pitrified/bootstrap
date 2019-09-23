842d948fb598c30a6dcc7deca3932bd3065f7e08
Pitrified <nobilipietro@gmail.com>
2019-09-23 18:23:27 +0200

    Add tip to setup tkinter/PIL

--- install_python.sh
+++ install_python.sh
@@ -3,8 +3,6 @@ pip3 install virtualenvwrapper
 mkdir ~/.virtualenvs
 source ~/.bashrc 
 
-pip3 install numpy
-
 echo "" >> ~/.bash_aliases.local
 echo "# Virtualenvs" >> ~/.bash_aliases.local
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bash_aliases.local
@@ -13,3 +11,10 @@ echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bash_aliases.local
 echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bash_aliases.local
 source ~/.bashrc 
 
+# SCI
+# pip3 install numpy
+
+# IMAGE
+# sudo apt install python3-tk
+# pip3 install Pillow
+# pip3 install exifread
