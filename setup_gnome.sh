d6a147f607b84d85d74e2fee3de1c137950efa81
Pitrified <nobilipietro@gmail.com>
2019-10-14 20:56:37 +0200

    Add option to setup alt tab

--- setup_gnome.sh
+++ setup_gnome.sh
@@ -9,6 +9,9 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.settings-daemon.plugins.media-keys home '<Super>e'
 gsettings set org.gnome.settings-daemon.plugins.media-keys www '<Super>w'
 
+# alt tab only shows windows in current workspace
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
 # top bar
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.interface clock-show-date true
