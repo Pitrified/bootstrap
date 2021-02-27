8e44c434a9437c441ebf67deaa165e3ce9bf707a
Pitrified <nobilipietro@gmail.com>
2021-02-27 22:51:20 +0100

    Add flake8 and mypy to every virtualenv

--- finish_python.sh
+++ finish_python.sh
@@ -1,2 +1,4 @@
 echo "pip3 install black" >> ~/.virtualenvs/postmkvirtualenv
 echo "pip3 install pytest" >> ~/.virtualenvs/postmkvirtualenv
+echo "pip3 install flake8" >> ~/.virtualenvs/postmkvirtualenv
+echo "pip3 install mypy" >> ~/.virtualenvs/postmkvirtualenv
