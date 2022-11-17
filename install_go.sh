814018cf91e597d894ab619fc1236b00822dddab
Pitrified <nobilipietro@gmail.com>
2022-11-17 20:35:13 +0100

    Upgrade package versions

--- install_go.sh
+++ install_go.sh
@@ -1,8 +1,9 @@
 mkdir ~/setup_bootstrap
 cd ~/setup_bootstrap
-wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
+
+wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
 
 echo "" >> ~/.bash_aliases.local
 echo "# Golang" >> ~/.bash_aliases.local
--- install_tools.sh
+++ install_tools.sh
@@ -9,24 +9,25 @@ echo "export PATH=\$PATH:~/.local/bin" >> ~/.bash_aliases.local
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
+mkdir ~/setup_bootstrap
+cd ~/setup_bootstrap
+
 # EXA
 # https://the.exa.website/
 # since Ubuntu 20.10
 # sadly this version does not include --git
 # sudo apt install exa -y
-# mkdir ~/setup_bootstrap
-# cd ~/setup_bootstrap
-# wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
-# unzip exa-linux-x86_64-v0.10.1.zip
+wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+unzip exa-linux-x86_64-v0.10.1.zip
 # mv exa-linux-x86_64 ~/.local/bin/exa # not anymore
-# cp bin/exa ~/.local/bin/ # something like this
+cp ~/setup_bootstrap/bin/exa ~/.local/bin/ # something like this
 
 # BAT
 # https://github.com/sharkdp/bat
 # since Ubuntu 19.10
-sudo apt install bat -y
-# wget https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
-# sudo dpkg -i bat_0.21.0_amd64.deb
+# sudo apt install bat -y
+wget https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb
+sudo dpkg -i bat_0.22.1_amd64.deb
 
 # HACK FONT
 # https://github.com/source-foundry/Hack
