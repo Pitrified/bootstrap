32d767643d1455748278b71f7ad7adb7a714b90b
Pitrified <nobilipietro@gmail.com>
2023-04-16 13:10:50 +0200

    Add chrome installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,3 @@ git clone https://github.com/Pitrified/bootstrap.git
 # check what ~ is
 echo ~
 
-# mkdir ~/setup_bootstrap
-# cd ~/setup_bootstrap
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
-
