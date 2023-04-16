ff047b5bfed9ea21400757fb5493aeeb86327785
Pitrified <nobilipietro@gmail.com>
2023-04-16 13:18:30 +0200

    Update git credential manager install script

--- install_git_credential_manager.sh
+++ install_git_credential_manager.sh
@@ -1,23 +1,27 @@
 # https://github.com/GitCredentialManager/git-credential-manager
 
+mkdir ~/setup_bootstrap
+cd ~/setup_bootstrap
+
 # misc deps
 sudo apt-get install wget ca-certificates
 
 # get the right release
 # https://github.com/GitCredentialManager/git-credential-manager/releases/latest
-wget https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb
-sudo dpkg -i gcmcore-linux_amd64.2.0.696.deb 
+GCM_V="2.0.935"
+wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v${GCM_V}/gcm-linux_amd64.${GCM_V}.deb
+sudo dpkg -i gcm-linux_amd64.${GCM_V}.deb 
 
 # configure will place things in ~/dotfiles/git/gitconfig.symlink
 # you might want to place them in ~/.gitconfig.local
-git-credential-manager-core configure
+git-credential-manager configure
 
-# if you are on WSL with no GUI, it will complain
-# use GPG pass
-# https://github.com/GitCredentialManager/git-credential-manager/blob/main/docs/credstores.md
-sudo apt install gnupg2 pass
-gpg --gen-key
-pass init 'name <email>'
-export GPG_TTY=$(tty)
-git push
-# now you need the PAT for this account
+# # if you are on WSL with no GUI, it will complain
+# # use GPG pass
+# # https://github.com/GitCredentialManager/git-credential-manager/blob/main/docs/credstores.md
+# sudo apt install gnupg2 pass
+# gpg --gen-key
+# pass init 'name <email>'
+# export GPG_TTY=$(tty)
+# git push
+# # now you need the PAT for this account
