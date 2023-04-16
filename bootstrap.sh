dd80f87f1af617d5f8bbe7ed260a3631acd38341
Pitrified <nobilipietro@gmail.com>
2023-04-16 12:21:34 +0200

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+sudo apt install git -y
+git clone https://github.com/Pitrified/bootstrap.git
+
+# check what ~ is
+echo ~
+
+# mkdir ~/setup_bootstrap
+# cd ~/setup_bootstrap
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+
