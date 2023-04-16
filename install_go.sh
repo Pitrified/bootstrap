31ef1327883c7dd264e8081793daac42a14da851
Pitrified <nobilipietro@gmail.com>
2023-04-16 12:20:04 +0200

    Add go version variable

--- install_go.sh
+++ install_go.sh
@@ -1,9 +1,11 @@
 mkdir ~/setup_bootstrap
 cd ~/setup_bootstrap
 
-wget https://go.dev/dl/go1.19.3.linux-amd64.tar.gz
+GO_VERSION="1.20.3"
+
+wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.19.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 
 # for fyne
 sudo apt install -y libgl1-mesa-dev xorg-dev
